ca5703dd27a05a5495cd25be21a8f7cb8cd3e2be
minwootak <69132431+minwootak@users.noreply.github.com>
2021-09-02 19:18:29 +0900

    Add files via upload
    Implemented features; file I/O, receive an input from user, Search with the input

---
+++ Data.java
@@ -0,0 +1,43 @@
+package project1;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Data
+{
+	public static void DataLoad(String Keyword)
+	{	
+		String line;
+		int row = 0;
+		int i = 0, j = 0;
+		String path = ""; //Put the location of covid-data.csv//
+		String[] index = new String[110000];
+		try
+		{
+			BufferedReader read = new BufferedReader(new FileReader(path));
+			while((line = read.readLine()) != null)
+			{
+				index[row] = line;
+				if(index[row].contains(Keyword))
+				{
+					System.out.println(index[row]);
+				}
+				row++;
+			}
+			read.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		System.out.println("Type a keyword you would like to search.");
+		Scanner sc = new Scanner(System.in);
+		String Keyword = sc.next();
+		sc.close();
+		DataLoad(Keyword);
+	}
+}
