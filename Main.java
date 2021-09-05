93c0a8d707084fe4f2dc1328c9e82936205afb91
minwootak <69132431+minwootak@users.noreply.github.com>
2021-09-05 18:58:18 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
-public class display {
-    public static void main(String[] args) {
-        Frame frame = new Frame();
-    }
+public class group25
+{
+    public static void main(String args[])
+    {
+        Frame frame = new Frame();
+        System.out.println("Type a keyword you would like to search.");
+        Scanner sc = new Scanner(System.in);
+        String Keyword = sc.next();
+        sc.close();
+        DataLoad(Keyword);
+    }
 }
